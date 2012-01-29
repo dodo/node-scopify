2461cdd450bdd0aa84852bdedfdf236ee8067f2f
dodo <dodo@blacksec.org>
2012-01-29 23:36:54 +0100

    expose createScope function, which now can autorequire and code injection

--- scopify.js
+++ scopify.js
@@ -1,6 +1,20 @@
 
-module.exports = function (bundle) {
-    bundle.register("post", function (src) {
-        return ";(function(){" + src + "}).call(this);";
-    });
+
+var createScope = function (opts) {
+    opts = opts || {};
+    return function (bundle) {
+        bundle.register("post", function (src) {
+            if (opts.inject)
+                src += ";" + opts.inject + ";";
+            if (opts.require)
+                src += ";require(" + opts.require + ");";
+            return ";(function(){" + src + "}).call(this);";
+        });
+    };
 };
+
+// exports
+
+module.exports = createScope();
+module.exports.createScope = createScope;
+
