ba68e60b711a98987ad292293757a5914cd0e9d1
dodo <dodo.the.last@gmail.com>
2012-05-25 00:52:57 +0200

    split inject into append and prepend

--- scopify.js
+++ scopify.js
@@ -4,10 +4,15 @@ var createScope = function (opts) {
     opts = opts || {};
     return function (bundle) {
         bundle.register("post", function (src) {
+            // backwards compatibility to scopify 0.2.1
             if (opts.inject)
                 src += ";" + opts.inject + ";";
+            if (opts.prepend)
+                src += ";" + opts.prepend + ";";
             if (opts.require)
                 src += ";require('" + opts.require + "');";
+            if (opts.append)
+                src += ";" + opts.append + ";";
             return ";(function(){" + src + "}).call(this);";
         });
     };
